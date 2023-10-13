cb44f06cf837a9fb8e66a957a784167ae003cb7d
kuussant <santeri.kuusisto@helsinki.fi>
2023-10-13 17:33:01 +0300

    Added bootstrap and modified templates

--- routes.py
+++ routes.py
@@ -1,9 +1,10 @@
 from app import app
-from flask import render_template, redirect, request
+from flask import render_template, redirect, request, url_for, session
 import subforums, discussions, messages, users, comments
 
 @app.route("/")
 def index():
+    session['url'] = url_for("index")
     list = subforums.get_list()
     user_id = users.session_user_id()
     print("USER ID", user_id)
@@ -12,6 +13,7 @@ def index():
 #Siirrytään subiin
 @app.route("/sub/<int:id>")
 def sub(id):
+    session['url'] = url_for("sub", id=id)
     print("WENT TO SUB", id)
     list = discussions.get_list_by_sub_id(id)
     sub = subforums.get_sub(id)
@@ -20,6 +22,7 @@ def sub(id):
 #Siirrytään uuden subin luontiin
 @app.route("/new_sub")
 def new_sub():
+    session['url'] = url_for("new_sub")
     return render_template("new_sub.html")
 
 #Luodaan subi
@@ -34,6 +37,7 @@ def create_sub():
 #Siirrytään keskustelun luontiin
 @app.route("/sub/<int:id>/new_discussion")
 def new_discussion(id):
+    session['url'] = url_for("new_discussion", id=id)
     return render_template("new_discussion.html", id=id)
 
 #Luodaan keskustelu
@@ -49,6 +53,7 @@ def create_discussion():
 #Keskustelu
 @app.route("/sub/<int:sub_id>/discussion/<int:disc_id>")
 def discussion(sub_id, disc_id):
+    session['url'] = url_for("discussion", sub_id=sub_id, disc_id=disc_id)
     print(f"================== SUB_ID = {sub_id} DISC_ID = {disc_id} USER_ID = {users.session_user_id()} ===================")
     list = comments.get_list_by_id(disc_id)
     disc = discussions.get_discussion(disc_id)
@@ -56,6 +61,7 @@ def discussion(sub_id, disc_id):
 
 @app.route("/sub/<int:sub_id>/discussion/<int:disc_id>/new_comment")
 def new_comment(sub_id, disc_id):
+    session['url'] = url_for("new_comment", sub_id=sub_id, disc_id=disc_id)
     print(f"==================== ADDING COMMENT SUB_ID = {sub_id} DISC_ID = {disc_id} ======================")
     return render_template("new_comment.html", sub_id=sub_id, disc_id=disc_id)
 
@@ -79,30 +85,36 @@ def login():
         password = request.form["password"]
         if users.login(username, password):
             print("LOGIN OK")
-            return redirect("/")
+            return redirect(session["url"])
         else:
             print("LOGIN NOT OK")
             return render_template("login.html")
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
+    error = None
     if request.method == "GET":
         return render_template("register.html")
     
     if request.method == "POST":
         username = request.form["username"]
         password = request.form["password"]
-        if users.register(username, password):
-            return redirect("/")
-        else:
-            return render_template("register.html")
+
+        if len(username) > 3 and password > 5:
+            if users.register(username, password):
+                return redirect(session["url"])
+            else:
+                error = "Username is taken"
+                return render_template("register.html")
+
+            
         
 @app.route("/logout")
 def logout():
     users.logout()
-    return redirect("/")
+    return redirect(session["url"])
 
 @app.route("/profile/<int:id>", methods=["GET", "POST"])
 def profile(id):
-    
+    session['url'] = url_for("profile", id=id)
     return render_template("profile.html")
